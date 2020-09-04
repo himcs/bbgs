b023d46c131244cdf1b30a3dc22bb5ce9dcd1b28
himcs <787673628@qq.com>
2020-09-04 10:17:00 +0800

    分页支持

--- admin/src/main/java/top/himcs/bbgs/admin/controller/TestController.java
+++ admin/src/main/java/top/himcs/bbgs/admin/controller/TestController.java
@@ -1,7 +1,15 @@
 package top.himcs.bbgs.admin.controller;
 
+import com.alibaba.druid.sql.PagerUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import top.himcs.bbgs.common.api.CommonResult;
 import top.himcs.bbgs.mbg.mapper.SystemAdminMapper;
@@ -12,25 +20,37 @@ import java.util.List;
 
 @RequestMapping("/test")
 @RestController
+@Api(tags = "测试")
 public class TestController {
+    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     SystemAdminMapper adminMapper;
 
-    @RequestMapping("/admin")
+    @GetMapping("/admin")
     public CommonResult<SystemAdmin> admin() {
         SystemAdmin systemAdmin = adminMapper.selectByPrimaryKey(1);
         return CommonResult.success(systemAdmin);
     }
 
-    @RequestMapping("/admins")
+    @GetMapping("/admins")
     public CommonResult<List<SystemAdmin>> two() {
         SystemAdminExample systemAdminExample = new SystemAdminExample();
         byte b = 0;
         systemAdminExample.createCriteria()
                 .andIsDelEqualTo(Byte.valueOf(b));
         systemAdminExample.or().andAccountIsNotNull();
-
         List<SystemAdmin> list = adminMapper.selectByExample(systemAdminExample);
         return CommonResult.success(list);
+    }
+
+    @GetMapping("/admins/page")
+    public CommonResult<PageInfo<SystemAdmin>> adminsPage(@RequestParam(defaultValue = "1") int p) {
+        SystemAdminExample systemAdminExample = new SystemAdminExample();
+        systemAdminExample.createCriteria().andAccountIsNotNull();
+        PageHelper.startPage(p, 10);
+        List<SystemAdmin> list = adminMapper.selectByExample(systemAdminExample);
+        PageInfo<SystemAdmin> pageInfo = new PageInfo<>(list);
+        return CommonResult.success(pageInfo);
     }
 }
